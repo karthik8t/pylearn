33013491508bd335e8a71242be0cb7df76e9d534
karthik8t <thatikondakarthik13@gmail.com>
2025-05-30 03:12:24 +0530

    added sub concept cards and concept page

--- pylearn/src/renderer/screens/concepts.tsx
+++ pylearn/src/renderer/screens/concepts.tsx
@@ -2,12 +2,15 @@ import React, {useEffect, useState} from 'react'
 import {ConceptSchema} from "shared/types";
 import {ScrollArea, ScrollBar} from "renderer/components/ui/scroll-area";
 import {Button} from "renderer/components/ui/button";
-import {Card, CardContent, CardFooter} from "renderer/components/ui/card";
+import {Card, CardContent, CardFooter, CardHeader} from "renderer/components/ui/card";
+import {ArrowUpAZ} from "lucide-react";
+import {useNavigate} from "react-router-dom";
 
 type ConceptType = 'Beginner' | 'Intermediate' | 'Advanced' | 'All' | 'Unlearned' | 'Bookmarks'
 const conceptTypes: ConceptType[] = ['Beginner', 'Intermediate', 'Advanced', 'All', 'Unlearned', 'Bookmarks']
 
 const Concepts = () => {
+  const navigate = useNavigate();
   const [concepts, setConcepts] = useState<ConceptSchema[]>([])
   const [conceptType, setConceptType] = useState<ConceptType>('All')
   const [expandedConcept, setExpandedConcept] = useState<ConceptSchema | undefined>(undefined)
@@ -45,8 +48,9 @@ const Concepts = () => {
           {
             concepts.map(concept => {
               return (
-                <div>
+                <div id={"concept-container"}>
                   <Card key={concept.id}
+                        onDoubleClick={() => navigate("/concept")}
                         className={"flex flex-row h-[250px] border-1 border-gray-200 rounded-3xl shadow-none"}>
                     <CardContent>
                       <h1 className={"font-bold"}>{concept.name}</h1>
@@ -68,7 +72,20 @@ const Concepts = () => {
                   <div>
                     {
                       expandedConcept && expandedConcept.id === concept.id && (
-                        <p>hello</p>
+                        <ScrollArea className="w-[900px]">
+                          <div className="flex w-max space-x-4 py-4">
+                            {concept.sub_concepts.map((sub_concept) => (
+                              <Card key={sub_concept.name} className={"w-[350px] h-[155px] gap-0"}>
+                                <CardHeader><ArrowUpAZ /></CardHeader>
+                                <CardContent className={"overflow-ellipsis overflow-hidden"}>
+                                  <h1 className={"font-bold"}>{sub_concept.name}</h1>
+                                  <p className={"text-gray-500 overflow-ellipsis"}>{sub_concept.shortDescription}</p>
+                                </CardContent>
+                              </Card>
+                            ))}
+                          </div>
+                          <ScrollBar orientation="horizontal"/>
+                        </ScrollArea>
                       )
                     }
                   </div>
