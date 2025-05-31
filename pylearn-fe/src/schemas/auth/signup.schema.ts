d6f819182d821f462d753389988a0c0db9b12339
karthik8t <thatikondakarthik13@gmail.com>
2025-05-31 10:34:46 +0530

    removed old projects

--- pylearn-fe/src/schemas/auth/signup.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const signupSchema = z.object({
-    email: z.string().email(),
-    username: z.string().min(3),
-    password: z.string().min(6)
-})
-
-export type SignupSchema = z.infer<typeof signupSchema>
